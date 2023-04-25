f0d1ca5cae4254d35c904ebb3f180801e4533f0e
Hubert Kolańczyk <kolanczykhubert@gmail.com>
2023-04-26 01:56:51 +0200

    Removed unnecessaryfile

--- project/src/Tests/CompileAndRunTest.sh
+++
@@ -1,4 +0,0 @@
-g++ -std=c++17 -Wall -Wextra -I/home/stary/.vcpkg/installed/x64-linux/include \
-    -L/home/stary/.vcpkg/installed/x64-linux/lib -lgtest -lnlohmann_json \
-    -lboost_date_time -lboost_filesystem UnitTest.cpp -o UnitTest
-./UnitTest
